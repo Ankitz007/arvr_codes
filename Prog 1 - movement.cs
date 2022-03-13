f436fc38cb006352fa86db96a9accb003da4c549
Ankit Kumar Singh <42801908+Ankitz007@users.noreply.github.com>
2022-03-13 23:22:20 +0530

    Create Prog 1 - movement.cs

---
+++ Prog 1 - movement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float speed;
+    void Start()
+    {
+        speed = 2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(speed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, 0f);
+    }
+}
